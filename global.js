46125c070812026ad3c9e20f3201374fbad46a9b
Matthew Conroy <mmconroy@gmail.com>
2020-04-01 12:27:23 -0400

    First Commit

--- global.js
+++ global.js
@@ -1,42 +1,41 @@
-
 // Welcome! Here's an example of a console logged response
-console.log(4 + 5)
+console.log(4 + 5);
 
 /***********************/
 /* Part 1: Expressions */
 /***********************/
 
 //1.) Log an expression that evaluates to the sum of two integers
-// your code here 
-console.log('answer 1');
+// your code here
+console.log("answer 1");
 
 // 2.) log an expression that evaluates to the difference of two integers
-// your code here 
-console.log('answer 2');
+// your code here
+console.log("answer 2");
 
 // 3.) Log an expression that concatenates the two strings "I'm writing" and "my first JavaScript code"
-// your code here 
-console.log('answer 3');
+// your code here
+console.log("answer 3");
 
 // 4.) Log an expression that evaluates to true or false using two numbers that are greater or less than eachother
-// your code here 
-console.log('answer 4');
+// your code here
+console.log("answer 4");
 
 // 5.) Log an expression that uses modulo, with a result of 4
 // your code here
-console.log('answer 5');
+console.log("answer 5");
 
 /**********************/
 /* Part 2: Statements */
 /**********************/
 
 // 6.) Assign a variable and console log the declaration
-// your code here 
-console.log('answer 6');
+// your code here
+console.log("answer 6");
 
 // 7.) Console log a variable assigned to the sum of two numbers
 // your code here
-console.log('answer 8');
+console.log("answer 8");
 
 // 8.) Uncomment this if else statement and add a logical expression as the condition
 //if (/*your code here*/) {
